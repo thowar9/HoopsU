5c0d1ad2f4769d919c7278d05b046181cb5f44ff
thowar9 <thowar9@students.towson.edu>
2017-12-11 18:33:02 -0500

    Logging Finished
    Log4net added. Exception is thrown when login page loads.

--- HoopsU/HoopsU/Pages/Login.aspx.cs
+++ HoopsU/HoopsU/Pages/Login.aspx.cs
@@ -6,14 +6,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using log4net; 
 
 namespace HoopsU
 {
     public partial class Login : System.Web.UI.Page
     {
+        private static readonly ILog logger = log4net.LogManager.GetLogger(
+            System.Reflection.MethodBase.GetCurrentMethod().DeclaringType); //for log4net
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            log4net.Config.XmlConfigurator.Configure(); 
+            try
+            {
+                DataSet ds = new DataSet(); //fake 
+                ds.ReadXml(MapPath(".") + "/../App_Data/PopulatePageDummy.xml"); //fake dummy file to throw error
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Login Population Error", ex); 
+            }
         }
 
         protected void Admin_Login_Click(object sender, EventArgs e)
@@ -22,7 +35,7 @@ namespace HoopsU
             try
             {
                 DataSet ds = new DataSet();
-                ds.ReadXml(MapPath(".") + "/../App_Data/LoginInfoDummy.xml"); 
+                ds.ReadXml(MapPath(".") + "/../App_Data/LoginInfoDummy.xml"); //fake dummy file to throw error
             }
             catch (Exception exc)
             {
